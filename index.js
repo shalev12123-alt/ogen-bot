9477aaddedd307b3a0a1839df8fe5ff735506f8d
shalev12123-alt <shalev12123@gmail.com>
2026-03-16 00:14:59 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -153,6 +153,7 @@ const client = new Client({
   authStrategy: new LocalAuth({ dataPath: './wa-session' }),
   puppeteer: {
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
@@ -161,7 +162,9 @@ const client = new Client({
       '--no-first-run',
       '--no-zygote',
       '--single-process',
-      '--disable-gpu'
+      '--disable-gpu',
+      '--disable-extensions',
+      '--disable-software-rasterizer',
     ],
   }
 });
